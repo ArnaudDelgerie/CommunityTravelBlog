ad5a1ae27bedb9123858c010c78fb15cb0bda34c
Arnaud <delgerie.arnaud@gmail.com>
2021-04-29 12:18:04 +0200

    Add upadtePost and deletePost methods to AuthUser/PostController

--- api/src/Controller/AuthUser/PostController.php
+++ api/src/Controller/AuthUser/PostController.php
@@ -8,6 +8,7 @@ use App\Service\Base64Service;
 use App\Repository\PostRepository;
 use App\Repository\CountryRepository;
 use App\Serializer\Schema\PostSchema;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -86,7 +87,7 @@ class PostController extends AbstractController
         $post->setCreatedBy($loggedUser);
         $post->setCreatedAt();
         $post->setActive(true);
-        $post->setValidated(false);
+        $post->setValidated(true);
 
         //check data error
         foreach ($this->validator->validate($post) as $violation) {
@@ -106,6 +107,75 @@ class PostController extends AbstractController
         $this->manager->flush();
 
         return new JsonResponse(["success" => true], 201);
+    }
+
+    /**
+     * @Route("/{id}", methods={"PATCH"})
+     */
+    public function updatePost($id, Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        //get logged user
+        $loggedUser = $this->getUser();
+
+        //check if post exist
+        $post = $this->postRepository->findOneBy(["id" => $id, "createdBy" => $loggedUser->getId()]);
+        if (!$post) {
+            return new JsonResponse(["success" => false, "message" => "post not found"], 401);
+        }
+
+        //check if country exist
+        $country = $this->countryRepository->find($data["country"] ?? 0);
+        if (!$country) {
+            return new JsonResponse(["success" => false, "message" => "country not found"], 401);
+        }
+
+        //update post
+        $post->setTitle($data["title"] ?? null);
+        $post->setContent($data["content"] ?? null);
+        $post->setRelatedCountry($country);
+
+        //check data error
+        foreach ($this->validator->validate($post) as $violation) {
+            if ($violation->getMessage()) {
+                return new JsonResponse(["success" => false, "message" => $violation->getMessage()], 401);
+            }
+        }
+
+        //save in db
+        $this->manager->persist($post);
+        $this->manager->flush();
+
+        return new JsonResponse(["success" => true], 201);
+    }
+
+    /**
+     * @Route("/{id}", methods={"DELETE"})
+     */
+    public function deletePost($id)
+    {
+        //get logged user
+        $loggedUser = $this->getUser();
+
+        //check if post exist
+        $post = $this->postRepository->findOneBy(["id" => $id, "createdBy" => $loggedUser->getId()]);
+        if (!$post) {
+            return new JsonResponse(["success" => false, "message" => "post not found"], 401);
+        }
+
+        //delete postImages to server
+        $images = $post->getPostImages();
+        foreach ($images as $image) {
+            $filesystem = new Filesystem();
+            $filesystem->remove($this->getParameter("post_img_dir") . "/" . $image->getImage());
+        }
+
+        //remove post and postImage to db
+        $this->manager->remove($post);
+        $this->manager->flush();
+
+        return new JsonResponse(["success" => true], 204);
     }
 
     private function decodeAndAddImage($data, &$post)
